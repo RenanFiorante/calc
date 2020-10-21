9b4f666a20838784c7c3b67836af21788f9c9de1
Renan Fiorante <renan.fiorante@usp.br>
2020-10-21 10:52:30 -0300

    Updates

---
+++ precache-manifest.675a2f1ff7ec8844beb02aeb902cd0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3709109dcb1e250f3b348b26747e76",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "22490ca4defe7ec8be52",
+    "url": "/calc/static/css/main.c9656c28.chunk.css"
+  },
+  {
+    "revision": "419fd1eb58bcd2ac1066",
+    "url": "/calc/static/js/2.9d566cbc.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/calc/static/js/2.9d566cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22490ca4defe7ec8be52",
+    "url": "/calc/static/js/main.c16a4b3a.chunk.js"
+  },
+  {
+    "revision": "8d305de9559aaa2757da",
+    "url": "/calc/static/js/runtime-main.0146263a.js"
+  }
+]);
